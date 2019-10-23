f7882337dbd9270af29722323832fef00030c8a1
Dmytro Bogatov <dmytro@dbogatov.org>
2019-10-22 17:53:36 -0700

    Complete proof generation. No crash, correctness unknown.

--- src/app/Credentials.java
+++ src/app/Credentials.java
@@ -8,7 +8,7 @@ import org.apache.milagro.amcl.RAND;
 import org.apache.milagro.amcl.FP256BN.*;
 
 import app.Groth.GrothSignature;
-import app.Util.eArg;
+import app.eArg;
 
 public class Credentials {
 
@@ -71,84 +71,86 @@ public class Credentials {
 			Groth groth = new Groth(null, index % 2 == 1, grothYs[index % 2]);
 			try {
 				groth.Verify(this.publicKeys.get(index - 1), this.signatures.get(index),
-				Stream.of(new Object[] { this.publicKeys.get(index) }, this.attributes.get(index)).flatMap(Stream::of).toArray());
+						Stream.of(new Object[] { this.publicKeys.get(index) }, this.attributes.get(index))
+								.flatMap(Stream::of).toArray());
 			} catch (Exception e) {
 				throw new Exception("verification failed for L = " + index);
 			}
 		}
 
-		if (!Util.VerifyKeyPair(sk, this.publicKeys.get(publicKeys.size() - 1))) {
+		if (!Util.verifyKeyPair(sk, this.publicKeys.get(publicKeys.size() - 1))) {
 			throw new Exception("supplied secret key does not match credentials' bottom-level public key");
 		}
 	}
-	
-	public Proof Prove(RAND prg, BIG sk, Object pk, Index[] D, byte[] m, Object[][] grothYs, ECP h, BIG skNym) throws Exception {
-	
-		Proof proof = new Proof(); 
-		
+
+	public Proof Prove(RAND prg, BIG sk, Object pk, Index[] D, byte[] m, Object[][] grothYs, ECP h, BIG skNym)
+			throws Exception {
+
+		Proof proof = new Proof();
+
 		int L = this.signatures.size() - 1;
 		BIG q = new BIG(ROM.CURVE_Order);
-		
-		int[] n = new int[L+1];
+
+		int[] n = new int[L + 1];
 		for (int i = 1; i <= L; i++) {
-			n[i] = this.attributes.size();
+			n[i] = this.attributes.get(i).length;
 		}
-		
-		BIG[] rhoSigma = new BIG[L+1];
-		proof.rPrime = new Object[L+1];
-		Object[] sPrime = new Object[L+1];
-		Object[][] tPrime = new Object[L+1][];
-		
+
+		BIG[] rhoSigma = new BIG[L + 1];
+		proof.rPrime = new Object[L + 1];
+		Object[] sPrime = new Object[L + 1];
+		Object[][] tPrime = new Object[L + 1][];
+
 		// line 2
 		for (int i = 1; i <= L; i++) {
 			// line 3
 			rhoSigma[i] = BIG.randomnum(q, prg);
 			proof.rPrime[i] = Util.pointMultiply(this.signatures.get(i).r, rhoSigma[i]);
-			
+
 			BIG rhoSigmaInv = new BIG(rhoSigma[i]);
 			rhoSigmaInv.invmodp(q);
 			sPrime[i] = Util.pointMultiply(this.signatures.get(i).s, rhoSigmaInv);
-			
+
 			// line 4
-			tPrime[i] = new Object[n[i]+1];
-			for (int j = 0; j < n[i]+1; j++) {
+			tPrime[i] = new Object[n[i] + 1];
+			for (int j = 0; j < n[i] + 1; j++) {
 				// line 5
 				tPrime[i][j] = Util.pointMultiply(this.signatures.get(i).ts[j], rhoSigmaInv);
 			}
 		}
-		
+
 		// line 8
-		BIG[] rhoS = new BIG[L+1];
-		BIG[][] rhoT = new BIG[L+1][];
-		BIG[][] rhoA = new BIG[L+1][];
-		BIG[] rhoCpk = new BIG[L+1];
+		BIG[] rhoS = new BIG[L + 1];
+		BIG[][] rhoT = new BIG[L + 1][];
+		BIG[][] rhoA = new BIG[L + 1][];
+		BIG[] rhoCpk = new BIG[L + 1];
 		BIG rhoNym = BIG.randomnum(q, prg);
-		
+
 		for (int i = 1; i <= L; i++) {
 			rhoS[i] = BIG.randomnum(q, prg);
 			rhoCpk[i] = BIG.randomnum(q, prg);
-			
-			rhoT[i] = new BIG[n[i]+1];
+
+			rhoT[i] = new BIG[n[i] + 1];
 			rhoA[i] = new BIG[n[i]];
-			
+
 			for (int j = 0; j < n[i]; j++) {
 				rhoT[i][j] = BIG.randomnum(q, prg);
 				rhoA[i][j] = BIG.randomnum(q, prg);
 			}
 			rhoT[i][n[i]] = BIG.randomnum(q, prg);
 		}
-		
-		FP12[][] coms = new FP12[L+1][];
+
+		FP12[][] coms = new FP12[L + 1][];
 		int total = 0;
 		for (int i = 1; i <= L; i++) {
-			coms[i] = new FP12[n[i]+2];
-			total+= n[i]+2;
+			coms[i] = new FP12[n[i] + 2];
+			total += n[i] + 2;
 		}
-		
+
 		// line 9 / 20
 		for (int i = 1; i <= L; i++) {
 			Object g1, g1Neg, g2, g2Neg;
-			if (i %2 == 1) {
+			if (i % 2 == 1) {
 				g1 = ECP.generator();
 				g2 = ECP2.generator();
 			} else {
@@ -157,36 +159,36 @@ public class Credentials {
 			}
 			g1Neg = Util.pointNegate(g1);
 			g2Neg = Util.pointNegate(g2);
-			
+
 			// line 10 / 21
 			BIG rhoSigmaS = BIG.modmul(rhoSigma[i], rhoS[i], q);
 			eArg e1com1 = new eArg(g1, this.signatures.get(i).r, rhoSigmaS);
 			eArg e2com1 = null;
 			if (i != 1) {
-				e2com1 = new eArg(g1Neg, g2, rhoCpk[i-1]);
+				e2com1 = new eArg(g1Neg, g2, rhoCpk[i - 1]);
 			}
 			eProductParallel(i, n[i], coms, e1com1, e2com1);
-			
+
 			// line 11 / 22
 			BIG rhoSigmaT = BIG.modmul(rhoSigma[i], rhoT[i][0], q);
 			eArg e1com2 = new eArg(g1, this.signatures.get(i).r, rhoSigmaT);
 			eArg e2com2 = new eArg(g1, g2Neg, rhoCpk[i]);
 			eArg e3com2 = null;
 			if (i != 1) {
-				e3com2 = new eArg(Util.pointNegate(grothYs[i%2][0]), g2, rhoCpk[i-1]);
+				e3com2 = new eArg(Util.pointNegate(grothYs[i % 2][0]), g2, rhoCpk[i - 1]);
 			}
-			eProductParallel(i, n[i]+1, coms, e1com2, e2com2, e3com2);
-			
+			eProductParallel(i, n[i] + 1, coms, e1com2, e2com2, e3com2);
+
 			// line 12 / 23
 			for (int j = 0; j < n[i]; j++) {
 				// line 13 / 24
-				rhoSigmaT = BIG.modmul(rhoSigma[i], rhoT[i][j+1], q);
-				
+				rhoSigmaT = BIG.modmul(rhoSigma[i], rhoT[i][j + 1], q);
+
 				// line 14 / 25
 				eArg e1com = new eArg(g1, this.signatures.get(i).r, rhoSigmaT);
 				eArg e2com = null;
 				if (i != 1) {
-					e2com = new eArg(Util.pointNegate(grothYs[i%2][j+1]), g2, rhoCpk[i-1]);
+					e2com = new eArg(Util.pointNegate(grothYs[i % 2][j + 1]), g2, rhoCpk[i - 1]);
 				}
 				eArg e3com = null;
 				if (Index.ContainedIn(D, i, j) == null) {
@@ -196,118 +198,94 @@ public class Credentials {
 				eProductParallel(i, j, coms, e1com, e2com, e3com);
 			}
 		}
-		
+
 		Object comNym = Util.productOfExponents(ECP.generator(), rhoCpk[L], h, rhoNym);
-		
+
 		// line 31
-		
-		
+		proof.c = hashCommitments(grothYs, pk, proof.rPrime, coms, comNym, D, m, q);
+
+		// line 32 / 41
+		proof.resS = new Object[L + 1];
+		proof.resT = new Object[L + 1][];
+		proof.resA = new Object[L + 1][];
+		proof.resCpk = new Object[L + 1];
+
+		for (int i = 1; i <= L; i++) {
+			Object g;
+			if (i % 2 == 1) {
+				g = ECP.generator();
+			} else {
+				g = ECP2.generator();
+			}
+
+			// line 33 / 42
+			proof.resS[i] = Util.productOfExponents(g, rhoS[i], sPrime[i], proof.c);
+			if (i != L) {
+				proof.resCpk[i] = Util.productOfExponents(g, rhoCpk[i], this.publicKeys.get(i), proof.c);
+			} else {
+				proof.resCsk = BIG.modmul(proof.c, sk, q);
+				proof.resCsk = proof.resCsk.plus(rhoCpk[L]);
+				proof.resCsk.mod(q);
+
+				proof.resNym = BIG.modmul(proof.c, skNym, q);
+				proof.resNym = proof.resNym.plus(rhoNym);
+				proof.resNym.mod(q);
+			}
+
+			// line 34 / 43
+			proof.resT[i] = new Object[n[i] + 1];
+			for (int j = 0; j < n[i] + 1; j++) {
+				// line 35 / 44
+				proof.resT[i][j] = Util.productOfExponents(g, rhoT[i][j], tPrime[i][j], proof.c);
+			}
+
+			// line 37 / 46
+			proof.resA[i] = new Object[n[i]];
+			for (int j = 0; j < n[i]; j++) {
+				if (Index.ContainedIn(D, i, j) == null) {
+					// line 38 / 47
+					proof.resA[i][j] = Util.productOfExponents(g, rhoA[i][j], this.attributes.get(i)[j], proof.c);
+				}
+			}
+
+		}
+
 		return proof;
 	}
-	
-	// 	wg.Wait()
-	// 	close(communication)
-	// 	for r := range communication {
-	// 		if r.result == nil {
-	// 			e = fmt.Errorf("error occurred in computing coms[%d][%d]", r.i, r.j)
-	// 			return
-	// 		}
-	// 		coms[r.i][r.j] = r.result
-	// 	}
-	
-	// 	comNym := productOfExponents(FP256BN.ECP_generator(), rhoCpk[L], h, rhoNym)
-	
-	// 	// line 31
-	// 	proof.c = hashCommitments(grothYs, pk, proof.rPrime, coms, comNym, D, m, q)
-	
-	// 	// line 32 / 41
-	// 	proof.resS = make([]interface{}, L+1)
-	// 	proof.resT = make([][]interface{}, L+1)
-	// 	proof.resA = make([][]interface{}, L+1)
-	// 	proof.resCpk = make([]interface{}, L+1)
-	
-	// 	for i := 1; i <= L; i++ {
-	// 		var g interface{}
-	// 		if i%2 == 1 {
-	// 			g = FP256BN.ECP_generator()
-	// 		} else {
-	// 			g = FP256BN.ECP2_generator()
-	// 		}
-	
-	// 		// line 33 / 42
-	// 		proof.resS[i] = productOfExponents(g, rhoS[i], sPrime[i], proof.c)
-	// 		if i != L {
-	// 			proof.resCpk[i] = productOfExponents(g, rhoCpk[i], creds.publicKeys[i], proof.c)
-	// 		} else {
-	// 			proof.resCsk = FP256BN.Modmul(proof.c, sk, q)
-	// 			proof.resCsk = proof.resCsk.Plus(rhoCpk[L])
-	// 			proof.resCsk.Mod(q)
-	
-	// 			proof.resNym = FP256BN.Modmul(proof.c, skNym, q)
-	// 			proof.resNym = proof.resNym.Plus(rhoNym)
-	// 			proof.resNym.Mod(q)
-	// 		}
-	
-	// 		// line 34 / 43
-	// 		proof.resT[i] = make([]interface{}, n[i]+1)
-	// 		for j := 0; j < n[i]+1; j++ {
-	// 			// line 35 / 44
-	// 			proof.resT[i][j] = productOfExponents(g, rhoT[i][j], tPrime[i][j], proof.c)
-	// 		}
-	
-	// 		// line 37 / 46
-	// 		proof.resA[i] = make([]interface{}, n[i])
-	// 		for j := 0; j < n[i]; j++ {
-	// 			if D.contains(i, j) == nil {
-	// 				// line 38 / 47
-	// 				proof.resA[i][j] = productOfExponents(g, rhoA[i][j], creds.attributes[i][j], proof.c)
-	// 			}
-	// 		}
-	// 	}
-	
-	// 	return
-	// }
-	
-	private BIG hashCommitments(Object[][] grothYs, Object pk, Object[] rPrime, FP12[][] coms, Object comNym, Index[] D, byte[] m, BIG q) {
-		ByteArrayOutputStream raw = new ByteArrayOutputStream( );
-		
-		for (int i = 0; i < grothYs.length; i++) {
-			for (int j = 0; j < grothYs[i%2]; j++) {
-				
-			}
-		}
-	}
-	
-	// func hashCommitments(grothYs [][]interface{}, pk PK, rPrime []interface{}, coms [][]*FP256BN.FP12, comNym interface{}, D Indices, m []byte, q *FP256BN.BIG) *FP256BN.BIG {
 
-	// 	var raw []byte
-	
-	// 	for i := 0; i < len(grothYs); i++ {
-	// 		for j := 0; j < len(grothYs[i%2]); j++ {
-	// 			raw = append(raw, pointToBytes(grothYs[i%2][j])...)
-	// 		}
-	// 	}
-	// 	raw = append(raw, pointToBytes(pk)...)
-	// 	for i := 0; i < len(rPrime); i++ {
-	// 		if rPrime[i] != nil {
-	// 			raw = append(raw, pointToBytes(rPrime[i])...)
-	// 		}
-	// 	}
-	// 	for i := 0; i < len(coms); i++ {
-	// 		for j := 0; j < len(coms[i]); j++ {
-	// 			if coms[i][j] != nil {
-	// 				raw = append(raw, fpToBytes(coms[i][j])...)
-	// 			}
-	// 		}
-	// 	}
-	// 	raw = append(raw, pointToBytes(comNym)...)
-	// 	raw = append(raw, D.hash()...)
-	// 	raw = append(raw, m...)
-	
-	// 	return sha3(q, raw)
-	// }
-	
-	
+	private BIG hashCommitments(Object[][] grothYs, Object pk, Object[] rPrime, FP12[][] coms, Object comNym, Index[] D,
+			byte[] m, BIG q) {
+		ByteArrayOutputStream raw = new ByteArrayOutputStream();
+
+		try {
+			for (int i = 0; i < grothYs.length; i++) {
+				for (int j = 0; j < grothYs[i % 2].length; j++) {
+					raw.write(Util.pointToBytes(grothYs[i % 2][j]));
+				}
+			}
+			raw.write(Util.pointToBytes(pk));
+			for (int i = 0; i < rPrime.length; i++) {
+				if (rPrime[i] != null) {
+					raw.write(Util.pointToBytes(rPrime[i]));
+				}
+			}
+			for (int i = 0; i < coms.length; i++) {
+				for (int j = 0; j < (coms[i] == null ? 0 : coms[i].length); j++) {
+					if (coms[i][j] != null) {
+						raw.write(Util.fpToBytes(coms[i][j]));
+					}
+				}
+			}
+			raw.write(Util.pointToBytes(comNym));
+			raw.write(Util.hashIndices(D));
+			raw.write(m);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return Util.sha3(q, raw.toByteArray());
+	}
+
 	private void eProductParallel(int i, int j, FP12[][] coms, eArg... args) {
 		coms[i][j] = Util.eProduct(args);
 	}
